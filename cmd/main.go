3c0891b054e97cc0d4ad62a0f349a8c237c4a706
Maoz Ovadia <maoz.ovadia@unity3d.com>
2024-05-30 14:52:42 +0300

    chore: change to lior domain

--- cmd/main.go
+++ cmd/main.go
@@ -101,7 +101,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "7d4baaac.github.io",
+		LeaderElectionID:       "7d4baaac.liorfranko.github.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
